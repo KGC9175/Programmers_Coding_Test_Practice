d0e6b23fdeb8957375c0f3dfc4d9dc59a3ad73cc
KGC9175 <rlcks273@naver.com>
2021-04-22 18:10:34 +0900

    2nd_commit(make_var.)

--- 다리를지나는트럭.py
+++ 다리를지나는트럭.py
@@ -2,3 +2,14 @@
 # Level : 2
 # Category : Stack, Queue
 
+bridge_length, weight = map(int, input().split())
+truck_weights = list(map(int, input().split()))
+answer = 0
+length = [0] * bridge_length
+
+
+'''
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
+'''
